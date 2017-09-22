bea8227e874d098c820b81b1c7033d795c227b4f
lw000 <373102227@qq.com>
2017-09-22 11:37:36 +0800

    update

--- libutils/lw_util.cpp
+++ libutils/lw_util.cpp
@@ -1,71 +1,17 @@
-#include "lw_util.h"
 #include <string.h>
+#include <stdlib.h>
+#include <utils.h>
 
-#if defined(_WIN32)
-
+#if defined(WIN32) || defined(_WIN32)
 #include <windows.h>
+#include <winsock2.h>
+#else
+#include <uuid/uuid.h>
+#include <sys/time.h>
+#include <iconv.h>
+#endif
 
-unsigned int lw_make_software_version(unsigned char major, unsigned char minor, unsigned short build) {
-	unsigned int version = major;
-	version = (version << 8);
-	version = (version) | minor;
-	version = (version << 16);
-	version = (version) | build;
-	return version;
-}
-
-void lw_software_version(unsigned int version, unsigned char &major, unsigned char &minor, unsigned short &build) {
-	build = (version & 0x0000FFFF);
-
-	version = (version >> 16);
-	minor = (version & 0x000000FF);
-
-	version = (version >> 8);
-	major = (version & 0x000000FF);
-}
-
-unsigned char lw_major_version(unsigned int version) {
-	version = (version >> 24);
-	return (version & 0x000000FF);
-}
-
-unsigned char lw_minor_version(unsigned int version) {
-	version = (version >> 16);
-	return (version & 0x000000FF);
-}
-
-unsigned short lw_build_version(unsigned int version) {
-	return (version & 0x0000FFFF);
-}
-
-unsigned long lw_hash_code(const char* c)
-{
-	unsigned long ret = 0;
-	long n;
-	unsigned long v;
-	int r;
-
-	if ((c == NULL) || (*c == '\0'))
-		return(ret);
-	/*
-	unsigned char b[16];
-	MD5(c,strlen(c),b);
-	return(b[0]|(b[1]<<8)|(b[2]<<16)|(b[3]<<24));
-	*/
-
-	n = 0x100;
-	while (*c)
-	{
-		v = n | (*c);
-		n += 0x100;
-		r = (int)((v >> 2) ^ v) & 0x0f;
-		ret = (ret << r) | (ret >> (32 - r));
-		ret &= 0xFFFFFFFFL;
-		ret ^= v*v;
-		c++;
-	}
-	return((ret >> 16) ^ ret);
-}
+#if defined(WIN32) || defined(_WIN32)
 
 std::wstring StringUtf8ToWideChar(const std::string& strUtf8)
 {
@@ -112,7 +58,6 @@ std::string StringWideCharToUtf8(const std::wstring& strWideChar)
 			printf("Wrong convert to Utf8 code:0x%x", GetLastError());
 		}
 	}
-
 	return ret;
 }
 
@@ -142,33 +87,38 @@ std::string UTF8StringToMultiByte(const std::string& strUtf8)
 	return ret;
 }
 
-char* utf8_to_gbk(const char* utf8)
+// UTF-8 to GB2312
+std::string utf8_to_gbk(const char* inbuf)
 {
-	int len = MultiByteToWideChar(CP_UTF8, 0, utf8, -1, NULL, 0);
+	int len = MultiByteToWideChar(CP_UTF8, 0, inbuf, -1, NULL, 0);
 	wchar_t* wstr = new wchar_t[len + 1];
 	memset(wstr, 0, len + 1);
-	MultiByteToWideChar(CP_UTF8, 0, utf8, -1, wstr, len);
+	MultiByteToWideChar(CP_UTF8, 0, inbuf, -1, wstr, len);
 	len = WideCharToMultiByte(CP_ACP, 0, wstr, -1, NULL, 0, NULL, NULL);
 	char* str = new char[len + 1];
 	memset(str, 0, len + 1);
 	WideCharToMultiByte(CP_ACP, 0, wstr, -1, str, len, NULL, NULL);
 	if (wstr) delete[] wstr;
-	return str;
+	std::string r(str);
+	if (str) delete[] str;
+	return r;
 }
 
-//GB2312µ½UTF-8µÄ×ª»»
-char* gbk_to_utf8(const char* gb2312)
+// GB2312 to UTF-8
+std::string gbk_to_utf8(const char* inbuf)
 {
-	int len = MultiByteToWideChar(CP_ACP, 0, gb2312, -1, NULL, 0);
+	int len = MultiByteToWideChar(CP_ACP, 0, inbuf, -1, NULL, 0);
 	wchar_t* wstr = new wchar_t[len + 1];
 	memset(wstr, 0, len + 1);
-	MultiByteToWideChar(CP_ACP, 0, gb2312, -1, wstr, len);
+	MultiByteToWideChar(CP_ACP, 0, inbuf, -1, wstr, len);
 	len = WideCharToMultiByte(CP_UTF8, 0, wstr, -1, NULL, 0, NULL, NULL);
 	char* str = new char[len + 1];
 	memset(str, 0, len + 1);
 	WideCharToMultiByte(CP_UTF8, 0, wstr, -1, str, len, NULL, NULL);
 	if (wstr) delete[] wstr;
-	return str;
+	std::string r(str);
+	if (str) delete[] str;
+	return r;
 }
 
 static double getfreq(void)
@@ -191,19 +141,133 @@ double gettime(void)
 		return (double)t.QuadPart / f;
 	}
 }
-
 #else
-#include <stdlib.h>
-#include <sys/time.h>
+static int code_convert(char *from_charset, char *to_charset, char *inbuf,
+		int inlen, char *outbuf, int outlen) {
+	iconv_t cd;
+	int rc;
+	char **pin = &inbuf;
+	char **pout = &outbuf;
 
-double gettime(void)
-{
-	struct timeval tv;
-	if (gettimeofday(&tv, NULL) < 0) return 0.0;
-	else return (double)tv.tv_sec + ((double)tv.tv_usec / 1000000.);
+	cd = iconv_open(to_charset, from_charset);
+	if (cd == 0)
+		return -1;
+	memset(outbuf, 0, outlen);
+	if (iconv(cd, pin, (size_t*) &inlen, pout, (size_t*) &outlen) == -1)
+		return -1;
+	iconv_close(cd);
+	return 0;
 }
 
+int utf8_to_gbk(char *inbuf, char *outbuf, int outlen) {
+	return code_convert((char *) "utf-8", (char *) "gb2312", inbuf, strlen(inbuf),
+			outbuf, outlen);
+}
+
+int gbk_to_utf8(char *inbuf, char *outbuf, size_t outlen) {
+	return code_convert((char *) "gb2312", (char *) "utf-8", inbuf, strlen(inbuf),
+			outbuf, outlen);
+}
+
+double gettime(void) {
+	struct timeval tv;
+	if (gettimeofday(&tv, NULL) < 0)
+		return 0.0;
+	else
+		return (double) tv.tv_sec + ((double) tv.tv_usec / 1000000.);
+}
 #endif
+
+std::string make_uuidstring() {
+#if defined(_WIN32) || defined(WIN32)
+	char buffer[64] = {0};
+	GUID guid;
+	if (CoCreateGuid(&guid))
+	{
+		fprintf(stderr, "create guid error\n");
+	}
+
+	_snprintf(buffer, sizeof(buffer),
+			"%08X-%04X-%04x-%02X%02X-%02X%02X%02X%02X%02X%02X",
+			guid.Data1, guid.Data2, guid.Data3,
+			guid.Data4[0], guid.Data4[1], guid.Data4[2],
+			guid.Data4[3], guid.Data4[4], guid.Data4[5],
+			guid.Data4[6], guid.Data4[7]);
+	return G2U(buffer));
+#else
+	uuid_t uu;
+	uuid_generate(uu);
+	char buf[64];
+	int n = 0;
+	for (int i = 0; i < 16; i++) {
+		n += sprintf(&buf[n], "%02X-", uu[i]);
+	}
+	std::string str_uuid(buf, strlen(buf) - 1);
+	return str_uuid;
+#endif // _WIN32
+}
+
+unsigned int lw_make_software_version(unsigned char major, unsigned char minor,
+		unsigned short build) {
+	unsigned int version = major;
+	version = (version << 8);
+	version = (version) | minor;
+	version = (version << 16);
+	version = (version) | build;
+	return version;
+}
+
+void lw_software_version(unsigned int version, unsigned char &major,
+		unsigned char &minor, unsigned short &build) {
+	build = (version & 0x0000FFFF);
+
+	version = (version >> 16);
+	minor = (version & 0x000000FF);
+
+	version = (version >> 8);
+	major = (version & 0x000000FF);
+}
+
+unsigned char lw_major_version(unsigned int version) {
+	version = (version >> 24);
+	return (version & 0x000000FF);
+}
+
+unsigned char lw_minor_version(unsigned int version) {
+	version = (version >> 16);
+	return (version & 0x000000FF);
+}
+
+unsigned short lw_build_version(unsigned int version) {
+	return (version & 0x0000FFFF);
+}
+
+unsigned long lw_hash_code(const char* c) {
+	unsigned long ret = 0;
+	long n;
+	unsigned long v;
+	int r;
+
+	if ((c == NULL) || (*c == '\0'))
+		return (ret);
+	/*
+	 unsigned char b[16];
+	 MD5(c,strlen(c),b);
+	 return(b[0]|(b[1]<<8)|(b[2]<<16)|(b[3]<<24));
+	 */
+
+	n = 0x100;
+	while (*c) {
+		v = n | (*c);
+		n += 0x100;
+		r = (int) ((v >> 2) ^ v) & 0x0f;
+		ret = (ret << r) | (ret >> (32 - r));
+		ret &= 0xFFFFFFFFL;
+		ret ^= v * v;
+		c++;
+	}
+	return ((ret >> 16) ^ ret);
+}
 
 char * lw_strtok_r(char *s, const char *delim, char **state) {
 	char *cp, *start;
@@ -217,35 +281,39 @@ char * lw_strtok_r(char *s, const char *delim, char **state) {
 			return NULL;
 		*state = NULL;
 		return start;
-	}
-	else {
+	} else {
 		*cp++ = '\0';
 		*state = cp;
 		return start;
 	}
 }
 
-KVQueryUrlValue::KVQueryUrlValue()
-{
+KVQueryUrlArgsValue::KVQueryUrlArgsValue() {
 	_kv.clear();
 }
 
-KVQueryUrlValue::~KVQueryUrlValue()
-{
+KVQueryUrlArgsValue::~KVQueryUrlArgsValue() {
 	this->reset();
 }
 
-int KVQueryUrlValue::parse(const char* data)
-{
+int KVQueryUrlArgsValue::parse(const char* data) {
+	if (data == NULL) {
+		return -1;
+	}
+
+	if (strlen(data) == 0) {
+		return -1;
+	}
+
 	this->reset();
 
 	char *p = const_cast<char*>(data);
 	char *p0 = NULL;
 	char *p1 = NULL;
 	p0 = lw_strtok_r(p, "&", &p1);
-	if (p0 == NULL) return -1;
-	while (p0 != NULL)
-	{
+	if (p0 == NULL)
+		return -1;
+	while (p0 != NULL) {
 		char *q = NULL;
 		char *q1 = NULL;
 
@@ -253,15 +321,17 @@ int KVQueryUrlValue::parse(const char* data)
 		std::string v;
 
 		q = lw_strtok_r(const_cast<char*>(p0), "=", &q1);
-		if (q == NULL) return -1;
+		if (q == NULL)
+			return -1;
 		k = q;
 		q = lw_strtok_r(NULL, "=", &q1);
-		if (q == NULL) return -1;
+		if (q == NULL)
+			return -1;
 		v = q;
 
-		KV *_pkv = (KV*)malloc(sizeof(KV));
-		_pkv->k = (char*)::malloc(k.size() + 1);
-		_pkv->v = (char*)::malloc(v.size() + 1);
+		KV_T *_pkv = (KV_T*) malloc(sizeof(KV_T));
+		_pkv->k = (char*) ::malloc(k.size() + 1);
+		_pkv->v = (char*) ::malloc(v.size() + 1);
 		strcpy(_pkv->k, k.c_str());
 		strcpy(_pkv->v, v.c_str());
 
@@ -272,35 +342,30 @@ int KVQueryUrlValue::parse(const char* data)
 	return 0;
 }
 
-char* KVQueryUrlValue::find(const char* key)
-{
-	std::list<KV*>::iterator iter = _kv.begin();
-	for (; iter != _kv.end(); ++iter)
-	{
-		KV *_pkv = *iter;
-		if (strcmp(_pkv->k, key) == 0)
-		{
-			return _pkv->v; break;
+char* KVQueryUrlArgsValue::find(const char* key) {
+	std::list<KV_T*>::iterator iter = _kv.begin();
+	for (; iter != _kv.end(); ++iter) {
+		KV_T *_pkv = *iter;
+		if (strcmp(_pkv->k, key) == 0) {
+			return _pkv->v;
+			break;
 		}
 	}
 	return NULL;
 }
 
-void KVQueryUrlValue::each(std::function<void(KV*)> func)
-{
-	std::list<KV*>::iterator iter = _kv.begin();
-	for (; iter != _kv.end(); ++iter)
-	{
-		KV *_pkv = *iter;
+void KVQueryUrlArgsValue::each(std::function<void(KV_T*)> func) {
+	std::list<KV_T*>::iterator iter = _kv.begin();
+	for (; iter != _kv.end(); ++iter) {
+		KV_T *_pkv = *iter;
 		func(_pkv);
 	}
 }
 
-void KVQueryUrlValue::reset() {
-	std::list<KV*>::iterator iter = _kv.begin();
-	for (; iter != _kv.end(); ++iter)
-	{
-		KV *_pkv = *iter;
+void KVQueryUrlArgsValue::reset() {
+	std::list<KV_T*>::iterator iter = _kv.begin();
+	for (; iter != _kv.end(); ++iter) {
+		KV_T *_pkv = *iter;
 		free(_pkv->k);
 		free(_pkv->v);
 		free(_pkv);
@@ -308,34 +373,30 @@ void KVQueryUrlValue::reset() {
 	_kv.clear();
 }
 
-std::vector<std::string> split(const char* str, const char* pattern)
-{
+std::vector<std::string> split(const char* str, const char* pattern) {
 	char *p = NULL;
 	char *p1 = NULL;
 	p = lw_strtok_r(const_cast<char*>(str), pattern, &p1);
 	std::vector<std::string> s;
-	while (p != NULL)
-	{
+	while (p != NULL) {
 		s.push_back(p);
 		p = lw_strtok_r(NULL, pattern, &p1);
 	}
 	return s;
 }
 
-std::unordered_map<std::string, std::string> split_url_pragma_data(const char* str)
-{
+std::unordered_map<std::string, std::string> split_url_pragma_data(
+		const char* str) {
 	char *p = const_cast<char*>(str);
 	char *p0 = NULL;
 	char *p1 = NULL;
 	p0 = lw_strtok_r(p, "&", &p1);
-	if (p0 == NULL)
-	{
+	if (p0 == NULL) {
 		return std::unordered_map<std::string, std::string>();
 	}
 
 	std::unordered_map<std::string, std::string> s;
-	while (p0 != NULL)
-	{
+	while (p0 != NULL) {
 		{
 			char *q = NULL;
 			char *q1 = NULL;
@@ -353,52 +414,84 @@ std::unordered_map<std::string, std::string> split_url_pragma_data(const char* s
 	return s;
 }
 
-void trim(char* src, char* dest)
-{
+void trim(char* src, char* dest) {
 	char* ps = src;
 	char* pe = src + strlen(src) - 1;
 
-	while (*ps == ' ') { ++ps;}
+	while (*ps == ' ') {
+		++ps;
+	}
 
-	while (*pe == ' ') { --pe;}
+	while (*pe == ' ') {
+		--pe;
+	}
 
 	char* p = dest;
-	while (ps <= pe)
-	{
+	while (ps <= pe) {
 		*p++ = *ps++;
 	}
 
 	*p = '\0';
 }
 
-void trim_l(char* src, char* dest)
-{
+void trim_l(char* src, char* dest) {
 	char* ps = src;
 	char* pe = src + strlen(src) - 1;
 
-	while (*ps == ' ') { ++ps; }
+	while (*ps == ' ') {
+		++ps;
+	}
 
 	char* p = dest;
-	while (ps <= pe)
-	{
+	while (ps <= pe) {
 		*p++ = *ps++;
 	}
 
 	*p = '\0';
 }
 
-void trim_r(char* src, char* dest)
-{
+void trim_r(char* src, char* dest) {
 	char* ps = src;
 	char* pe = src + strlen(src) - 1;
 
-	while (*pe == ' ') { --pe; }
+	while (*pe == ' ') {
+		--pe;
+	}
 
 	char* p = dest;
-	while (ps <= pe)
-	{
+	while (ps <= pe) {
 		*p++ = *ps++;
 	}
 
 	*p = '\0';
 }
+
+int lw_socket_init() {
+#if defined(WIN32) || defined(_WIN32)
+	WORD wVersionRequested;
+	WSADATA wsaData;
+	wVersionRequested = MAKEWORD(2, 2);
+	int err = WSAStartup(wVersionRequested, &wsaData);
+	if (err != 0)
+	{
+		printf("WSAStartup failed with error: %d\n", err);
+		return 0;
+	}
+#endif
+	return 0;
+}
+
+void lw_socket_clean() {
+#if defined(WIN32) || defined(_WIN32)
+	WSACleanup();
+#endif
+}
+
+SocketInit::SocketInit() {
+	lw_socket_init();
+}
+
+SocketInit::~SocketInit() {
+	lw_socket_clean();
+}
+
--- libutils/lw_util.h
+++ libutils/lw_util.h
@@ -1,46 +1,52 @@
 #ifndef __LW_UTIL_H__
 #define __LW_UTIL_H__
 
-#include <vector>
-#include <string>
-#include <unordered_map>
-#include <list>
-
-#ifdef _WIN32
+#if defined(WIN32) || defined(_WIN32)
 #include <windows.h>
 #include <process.h>
 #else
 #include <unistd.h>
 #endif
 
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <list>
 #include <functional>
 
-#ifdef _WIN32
+#if defined(WIN32) || defined(_WIN32)
 #define lw_sleep(seconds) SleepEx(seconds * 1000, 1);
 #else
 #define lw_sleep(seconds) sleep(seconds);
 #endif
 
-struct KV
+struct KV_T
 {
 	char *k;
 	char *v;
 };
 
-class KVQueryUrlValue
+class KVQueryUrlArgsValue
 {
 public:
-	KVQueryUrlValue();
-	~KVQueryUrlValue();
+	KVQueryUrlArgsValue();
+	~KVQueryUrlArgsValue();
 
 public:
 	int parse(const char* data);
 	char* find(const char* key);
-	void each(std::function<void(KV*)> func);
+	void each(std::function<void(KV_T*)> func);
 	void reset();
 
 private:
-	std::list<KV*> _kv;
+	std::list<KV_T*> _kv;
+};
+
+class SocketInit final
+{
+public:
+	SocketInit();
+	~SocketInit();
 };
 
 unsigned int lw_make_software_version(unsigned char major, unsigned char minor, unsigned short build);
@@ -55,34 +61,31 @@ char * lw_strtok_r(char *s, const char *delim, char **state);
 
 std::vector<std::string> split(const char* str, const char* pattern);
 
+std::string make_uuidstring();
+
 std::unordered_map<std::string, std::string> split_url_pragma_data(const char* str);
 
 void trim(char* src, char* dest);
 void trim_l(char* src, char* dest);
 void trim_r(char* src, char* dest);
 
-#if defined(_WIN32)
+#define lw_min(a,b) ((a)<(b)?(a):(b))
 
-//UTF-8��GB2312ת��
-char* utf8_to_gbk(const char* utf8);
-//GB2312��UTF-8ת��
-char* gbk_to_utf8(const char* gb2312);
+#define lw_max(a,b) ((a)>(b)?(a):(b))
+
+extern double gettime(void);
+
+#if defined(WIN32) || defined(_WIN32)
+
+//UTF-8 to GB2312
+std::string utf8_to_gbk(const char* inbuf);
+//GB2312 to UTF-8
+std::string gbk_to_utf8(const char* inbuf);
 
 std::wstring StringUtf8ToWideChar(const std::string& strUtf8);
 std::string StringWideCharToUtf8(const std::wstring& strWideChar);
 std::string UTF8StringToMultiByte(const std::string& strUtf8);
 
-#endif
-
-#if defined(_WIN32)
-#define U2G(v)	utf8_to_gbk(v)
-#define G2U(v)	gbk_to_utf8(v)
-#else
-#define U2G(v)
-#define G2U(v)
-#endif
-
-#ifdef _WIN32
 #ifndef __MINGW32__
 #include <stdio.h>
 #define snprintf(str, n, format, ...)  \
@@ -90,12 +93,17 @@ std::string UTF8StringToMultiByte(const std::string& strUtf8);
 #endif
 #define strcasecmp stricmp
 #define strncasecmp strnicmp
+
+#define U2G(v)	utf8_to_gbk(v)
+#define G2U(v)	gbk_to_utf8(v)
+
+#else
+
+#define U2G(v)
+#define G2U(v)
+
 #endif
 
-#define lw_min(a,b) ((a)<(b)?(a):(b))
 
-#define lw_max(a,b) ((a)>(b)?(a):(b))
-
-extern double gettime(void);
 
 #endif // !__LW_UTIL_H__
